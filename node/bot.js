bd8f069e3144c655d57ae7dcf4a9a9a16c8149f2
Shashank Shekhar <shashankshekhar1997@outlook.com>
2017-07-27 19:01:31 +0530

    falana chilana!

---
+++ node/bot.js
@@ -0,0 +1,11 @@
+console.log("hello bot");
+
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+//  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
