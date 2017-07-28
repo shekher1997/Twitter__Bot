a5d8240e14b90b57978cda388c468e609d9f4496
Shashank Shekhar <shashankshekhar1997@outlook.com>
2017-07-28 07:47:09 +0530

    Delete config.js

--- node/config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  consumer_key:         'IirgByF9T5Za9PXiyt2xt13QK',
-  consumer_secret:      'GgDCLvCy3abgULPyhPxnxdh5ycSHuOZJsE67yqpAFI4m0XYDwO',
-  access_token:         '890563213645017088-npankws6vXdVEeXM9VZKkEKHvgrrwS4',
-  access_token_secret:  'S7o7AGr7HifgBHNWYN4nsQBcSgXtYQ161nzxS81fgCBrO',
-//  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
