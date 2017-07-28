14034349dcc963ecdfc3d1232942f50c2271bcb7
Shashank Shekhar <shashankshekhar1997@outlook.com>
2017-07-28 07:26:13 +0530

    added config file

--- node/bot.js
+++ node/bot.js
@@ -1,11 +1,17 @@
 console.log("hello bot");
 
 var Twit = require('twit');
+var config = require('./config');
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-//  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+var T = new Twit(config);
+
+var params = {
+  q: '#jadavpuruniversity',
+  count: 2
+}
+
+T.get('search/tweets', params, goData);
+
+function goData(err, data, response) {
+  console.log(data);
+}
