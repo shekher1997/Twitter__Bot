3bf4beba18c43da5d897fe4a130465e5198d2b30
Shashank Shekhar <shashankshekhar1997@outlook.com>
2017-08-06 08:44:22 +0530

    added stream function in bot2.js

--- Bot/bot2.js
+++ Bot/bot2.js
@@ -5,26 +5,50 @@ console.log("Twitter Bot Initiating...");
 
 var Twit = require('twit');
 var config = require('./config');
-
 var T = new Twit(config);
 
-var postparams = {
-     status: 'Aha!My twitter bot is working.'
+
+//setting up a user stream.
+var stream = T.stream('user');
+
+//anytime someone follows me.
+stream.on('follow',followed);
+
+function followed(eventMsg) {
+    console.log('Follow event.')
+    var name = eventMsg.source.name;
+    var screenName = eventMsg.source.screen_name;
+    tweetIt('. @' + screenName + 'thanks for following.');
 }
 
-T.post('statuses/update', postparams, postData);
 
-function postData(err, data, response) {
-    // if (err){
-    //     // console.log("Something went wrong!")
-    //     console.log(data);
-    // }
-    // else{
-    //     console.log("Done!");
-    // }
-   console.log(data); 
+
+// tweetIt();
+// setInterval(tweetIt, 1000 * 20);
+
+function tweetIt(txt) {
+    // var r = Math.floor(Math.random() * 100);
+
+    var postparams = {
+        // status: r + ' Aha!My twitter bot is working.'
+        status: txt
+    }
+
+    T.post('statuses/update', postparams, postData);
+
+    function postData(err, data, response) {
+        // if (err){
+        //     // console.log("Something went wrong!")
+        //     console.log(data);
+        // }
+        // else{
+        //     console.log("Done!");
+        // }
+        console.log(data);
+    }
+
+    // T.post('statuses/update', { status: 'twitterbot' }, function(err, data, response) {
+    //   console.log(data)
+    // })
+
 }
-
-// T.post('statuses/update', { status: 'twitterbot' }, function(err, data, response) {
-//   console.log(data)
-// })
